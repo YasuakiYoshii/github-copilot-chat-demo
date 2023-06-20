099fb3d72537ec2f992b47b4d4cea18c01dd2d0b
吉井 尉哲 <y3-yoshii@nri.co.jp>
2023-06-20 17:35:48 +0900

    third commit

--- docs/index.js
+++ docs/index.js
@@ -1,10 +1,12 @@
-// Set up the canvas and context
-const canvas = document.getElementById('snake');
+// Set up the canvas
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+canvas.width = 400;
+canvas.height = 400;
 
 // Set up the game variables
 let snake = [{x: 10, y: 10}];
-let food = {x: 5, y: 5};
+let food = {x: Math.floor(Math.random() * canvas.width / 10), y: Math.floor(Math.random() * canvas.height / 10)};
 let direction = 'right';
 
 // Set up the game loop
@@ -27,22 +29,33 @@ function gameLoop() {
   }
   snake.unshift(head);
 
-  // Check for collision with food
+  // Check for collision with the food
   if (head.x === food.x && head.y === food.y) {
-    food.x = Math.floor(Math.random() * canvas.width);
-    food.y = Math.floor(Math.random() * canvas.height);
+    // Generate new food
+    food.x = Math.floor(Math.random() * canvas.width / 10);
+    food.y = Math.floor(Math.random() * canvas.height / 10);
   } else {
+    // Remove the tail segment
     snake.pop();
   }
 
-  // Check for collision with walls
-  if (head.x < 0 || head.x >= canvas.width || head.y < 0 || head.y >= canvas.height) {
+  // Check for collision with the walls or the snake's body
+  if (head.x < 0 || head.x >= canvas.width / 10 ||
+      head.y < 0 || head.y >= canvas.height / 10 ||
+      snake.slice(1).some(segment => segment.x === head.x && segment.y === head.y)) {
     clearInterval(intervalId);
     alert('Game over!');
+    // Reset the game variables
+    snake = [{x: 10, y: 10}];
+    food = {x: Math.floor(Math.random() * canvas.width / 10), y: Math.floor(Math.random() * canvas.height / 10)};
+    direction = 'right';
+    intervalId = setInterval(gameLoop, 100);
   }
 
-  // Draw the game
+  // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the game
   ctx.fillStyle = 'green';
   snake.forEach(segment => {
     ctx.fillRect(segment.x * 10, segment.y * 10, 10, 10);
